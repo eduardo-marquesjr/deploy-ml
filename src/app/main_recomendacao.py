2767c085bd515883be1141b42534903dc16d8fbd
emjunior <77761408+emjunior-ccm@users.noreply.github.com>
2021-03-29 09:10:18 -0300

    commit 29

--- src/app/main_recomendacao.py
+++ src/app/main_recomendacao.py
@@ -3,6 +3,8 @@ from flask_basicauth import BasicAuth
 import pandas as pd
 import os
 
+SECRET_KEY = 'ccm'
+
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = os.environ['BASIC_AUTH_USERNAME']
 app.config['BASIC_AUTH_PASSWORD'] = os.environ['BASIC_AUTH_PASSWORD']
@@ -11,10 +13,13 @@ basic_auth = BasicAuth(app)
 
 dados_final = pd.read_excel('Dados Final Potenza.xlsx') 
 dados_nomes = pd.read_excel('Dados Nomes Potenza.xlsx') 
+dados_usuarios = pd.read_csv('potenza.csv')
 
-@app.route('/') 
-@basic_auth.required
-def index():
+@app.route('/home') 
+# @basic_auth.required
+def home():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('home')))  
     lista_contas = sorted(list(dados_final['Conta'].unique()))  
     return render_template('visual_potenza.html', contas = lista_contas)
 
@@ -30,6 +35,31 @@ def recomenda():
     recomendacoes = recomendacoes[['Produto','Segmento','Categoria']]
     return render_template('recomendacao.html', dados_filtrado = dados_filtrado, 
                                     colunas = colunas, recomendacoes = recomendacoes)
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login_potenza.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    localiza_usuario = dados_usuarios[dados_usuarios['id'] == usuario]
+    if  localiza_usuario['senha'].values == senha:
+        session['usuario_logado'] = usuario
+        # flash(usuario.nome + ' logou com sucesso!') 
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
+    else:
+        flash('Não logado, tente novamente!')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=100, host='0.0.0.0')     
