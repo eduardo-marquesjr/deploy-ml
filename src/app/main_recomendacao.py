da3f84084ff367fef810146a98a0ff194491c7f5
emjunior-ccm <dudumarquesjr@hotmail.com>
2021-05-18 12:56:31 +0000

    depploy balance 1

--- src/app/main_recomendacao.py
+++ src/app/main_recomendacao.py
@@ -1,13 +1,16 @@
 #encoding: utf-8
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 import pandas as pd
+import datetime as dt
+import numpy as np
 import os
 from tratamento import trata_e_roda
+from pandas_datareader import data as pdr
 
 app = Flask(__name__)
 app.secret_key = 'ccm'
 
-dados_nomes, dados_usuarios = trata_e_roda()
+dados_nomes, dados_usuarios, base_btg_produtos, dados_precos, retorno_anual, cov_anual = trata_e_roda()
 
 @app.route('/home') 
 def home():
@@ -21,15 +24,56 @@ def recomenda():
     conta = int(request.form['conta']) 
     dados_filtrado = dados_nomes[['Conta','Tipo','Profissao', 'Estado_Civil', 'Estado', 'Perfil_do_Cliente',
                                  'Tipo_Investidor', 'Faixa_Cliente', 'Idade']][dados_nomes['Conta'] == conta]
-    localiza = dados_nomes[dados_nomes['Conta'] == conta]
+    dados_filtrado2 = base_btg_produtos[['Conta', 'Mercado', 'Produto', 'Segmento', 'Ativo', 
+                                         'Categoria']][base_btg_produtos['Conta'] == conta]
+    localiza = dados_nomes[dados_nomes['Conta'] == conta] 
     colunas = dados_filtrado.columns.values  
+    colunas2 = dados_filtrado2.columns.values
     cluster = localiza.Clusters.unique()[0] 
     recomendacoes = dados_nomes[dados_nomes.Clusters == cluster] 
     recomendacoes = recomendacoes['Categoria-Segmento'][recomendacoes.Clusters == cluster].unique()
     recomendacoes = [recomendacoes[i] for i in range(len(recomendacoes))] 
     tamanho_recomendacao = len(recomendacoes) 
+    
+    produtos_carteira = np.unique(base_btg_produtos[['Produto']][(base_btg_produtos['Conta'] == conta) 
+            & ((base_btg_produtos['Mercado'] == 'Derivativos') | (base_btg_produtos['Mercado'] == 'Renda Variável'))].values)
+    produtos_carteira = produtos_carteira + '.SA'
+    produtos_carteira1 = [produtos_carteira[i] for i in range(len(produtos_carteira)) if produtos_carteira[i] in
+                          dados_precos.columns.values] 
+    port_returns = []
+    port_volatility = [] 
+    stock_weights = []
+    lista_sharpe_ratio = []
+
+    num_assets = len(produtos_carteira1)
+    num_portfolios = 1000
+
+    for single_portfolio in range(num_portfolios):
+        pesos_carteira = np.random.random(num_assets)
+        pesos_carteira /= np.sum(pesos_carteira)
+        returns = np.dot(pesos_carteira, retorno_anual.loc[produtos_carteira1])
+        volatility = np.sqrt(np.dot(pesos_carteira.T, np.dot(cov_anual[produtos_carteira1].loc[produtos_carteira1], pesos_carteira)))
+        port_returns.append(returns) 
+        port_volatility.append(volatility)
+        stock_weights.append(pesos_carteira) 
+        sharpe_ratio = returns / volatility
+        lista_sharpe_ratio.append(sharpe_ratio) 
+
+    portfolio = {'Retornos' : port_returns, 'Volatilidade' : port_volatility, 'Sharpe' : lista_sharpe_ratio}
+
+    for counter, symbol in enumerate(produtos_carteira1):
+        portfolio[symbol + ' peso'] = [pesos_carteira[counter] for pesos_carteira in stock_weights]
+
+    df = pd.DataFrame(portfolio) 
+                          
+    maior_sharpe = df['Sharpe'].max()
+    carteira_maior_sharpe = df.loc[df['Sharpe'] == maior_sharpe] 
+    colunas3 = carteira_maior_sharpe.columns.values
+    
     return render_template('recomendacao.html', dados_filtrado = dados_filtrado, colunas = colunas, 
-                                    recomendacoes = recomendacoes, tamanho_recomendacao = tamanho_recomendacao)
+                                    recomendacoes = recomendacoes, tamanho_recomendacao = tamanho_recomendacao,
+                                    dados_filtrado2 = dados_filtrado2, colunas2 = colunas2,
+                                      carteira_maior_sharpe = carteira_maior_sharpe, colunas3 = colunas3) 
 
 @app.route('/login')
 def login():
--- src/app/tratamento.py
+++ src/app/tratamento.py
@@ -7,6 +7,7 @@ from sklearn.cluster import KMeans
 from sklearn import metrics 
 import warnings
 warnings.filterwarnings('ignore')
+from pandas_datareader import data as pdr
 
 def trata_e_roda():
     base_btg_produtos1 = 'SELECT * FROM `pristine-bonito-301012.ccmteste.pos`'
@@ -87,6 +88,39 @@ def trata_e_roda():
     dados2 = pd.get_dummies(columns = ['Tipo','Profissao','Estado_Civil', 'Estado',
         'Perfil_do_Cliente', 'Tipo_Investidor', 'Faixa_Cliente', 'Idade'], data = dados)
     dados3 = dados2.drop('Conta', axis = 1)
+    
+    carteira = []
+    for i in range(base_btg_produtos.shape[0]):
+        if base_btg_produtos['Mercado'][i:i+1][i] == 'Derivativos' or base_btg_produtos['Mercado'][i:i+1][i] == 'Renda Variável':
+            carteira.append(base_btg_produtos['Produto'][i:i+1][i]) 
+    carteira = np.unique(carteira)
+    carteira = carteira.astype('O') 
+    for j in range(len(carteira)):
+        carteira[j] = carteira[j] + '.SA'
+
+    yesterday = dt.date.today() - dt.timedelta(1)
+    yesterday = str(yesterday) 
+    dados_precos = pd.DataFrame() 
+    not_find = ['ABCB2.SA', 'BBASN361.SA', 'BBDCC285.SA', 'BBDCC293.SA', 'BBDCN234.SA', 'BBDCN256.SA',
+               'BBDCN271.SA', 'BBDCO197.SA', 'BBDCO256.SA', 'BOVAN110.SA', 'BRMLN820.SA',
+               'CNTO3.SA', 'COGNB480.SA', 'COGNC40.SA', 'COGNC750.SA', 'CPTS12.SA', 'CSNAC389.SA', 
+               'CSNAN32.SA', 'CSNAO329.SA', 'CVBI12.SA', 'CYREB320.SA', 'DMMO11.SA', 'GGBRB268.SA',
+               'GGBRB280.SA', 'GGBRC267.SA', 'GGBRN251.SA','GGBRO221.SA','GGBRO251.SA','IRBRB105.SA',
+               'IRBRB760.SA', 'IRBRB800.SA', 'IRBRC107.SA', 'IRBRC115.SA', 'IRBRN610.SA', 'ITUBC301.SA',
+               'ITUBN297.SA', 'ITUBO252.SA', 'ITUBO292.SA', 'JBSSB270.SA', 'LAME1.SA', 'LAME2.SA',
+               'OUJP12.SA', 'PETRB299.SA', 'PETRB317.SA', 'PETRC309.SA', 'PTAX800.SA', 'RLOG3.SA',
+               'VALEN937.SA','VALEO922.SA', 'VALEO962.SA', 'VILG14.SA', 'VIVR1.SA', 'VVARB155.SA',
+               'VVARB160.SA', 'VVARC155.SA', 'VVARC170.SA']          
+    for ativo in carteira: 
+        if ativo not in not_find:
+            dados_precos[ativo] = pdr.DataReader(ativo, data_source = 'yahoo', start = '2014-01-01')['Adj Close'] 
+            
+    dados_precos = dados_precos.fillna(method = 'bfill') 
+    retorno = dados_precos.pct_change()
+    retorno = retorno.iloc[1:] 
+    retorno_anual = retorno.mean() * 250
+    cov_diaria = retorno.cov()
+    cov_anual = cov_diaria * 250
 
     SEED = 42 
     np.random.seed(SEED)
@@ -104,4 +138,4 @@ def trata_e_roda():
     dados_usuarios = pd.read_gbq('SELECT * FROM `pristine-bonito-301012.ccmteste.login`')
     dados_usuarios = dados_usuarios.drop_duplicates()
     dados_usuarios = dados_usuarios.reset_index()
-    return dados_nomes, dados_usuarios
+    return dados_nomes, dados_usuarios, base_btg_produtos, dados_precos, retorno_anual, cov_anual
